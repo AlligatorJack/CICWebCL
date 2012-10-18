e6be832e46803f5cb22f78d015b458eaf8851974
AlligatorJack <alligator_jack@hotmail.com>
2012-10-18 17:20:56 +0200

    WebCLI.html + some more initialization

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -5,8 +5,8 @@ import play.api.mvc._
 
 object Application extends Controller {
   
-  def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+  def index(bla: String) = Action {
+    Ok(views.html.index.render("Your new application is super ready."+bla, 5))
   }
   
 }
