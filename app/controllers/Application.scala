053b821628fed7fb588076f6f36b468f6640f4ef
Evgeny Novoseltsev <evgeny_novoseltsev@web.de>
2012-10-22 12:24:50 +0200

    cic and cicwebcl linked, parser imported

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -3,10 +3,14 @@ package controllers
 import play.api._
 import play.api.mvc._
 import cmd.partial.PartialCmdParser
+import cmd.partial.PartialCmdLexer
+import java.io.StringReader
+import cmd.ast.Expr
 
 object Application extends Controller {
   
   def index(bla: String) = Action {
+    val s = new PartialCmdParser().parse(new PartialCmdLexer(new StringReader(bla))).asInstanceOf[Expr]
     Ok(views.html.index.render("Your new application is super ready."+bla, 5))
   }
   
