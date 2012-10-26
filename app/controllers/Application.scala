f620712829c8658a5e9c3676ffea9fca362107f4
Evgeny Novoseltsev <evgeny_novoseltsev@web.de>
2012-10-26 17:45:28 +0200

    view test

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -11,6 +11,7 @@ import cmd.partial.{ast => PAST}
 import play.api.templates.Html
 import scala.xml._
 import play.api.libs.iteratee.Enumerator
+import scala.util.regexp.SyntaxError
 
 object Application extends Controller {
   
@@ -18,30 +19,33 @@ object Application extends Controller {
     Ok(views.html.test.render())
   }
   
-//  def string(s: String)= Action {
-//    val expr = new PartialCmdParser().parse(new PartialCmdLexer(new StringReader(s))).asInstanceOf[Expr]
-//    Ok(views.html.test.render(colorize(expr)))
-//  }
-  
-  def stringAs(s: String) = Action {
-    Ok(colorize(new PartialCmdParser().parse(new PartialCmdLexer(new StringReader(s))).asInstanceOf[Expr]).toString)
+  def colorizeString(s: String)= Action {
+    val lexer = new PartialCmdLexer(new StringReader(s))
+    try {
+      val expr = new PartialCmdParser().parse(lexer).asInstanceOf[Expr]
+      Ok(colorizeExpr(expr).toString)
+    }
+    catch {
+      //there is no exception if beaver can repair the user-string. We need to override Events.syntaxError maybe(just throw an Exception)
+      case e:Exception => Ok(s)
+    }
   }
   
   def javascriptRoutes = Action { implicit request =>
     Ok(
       Routes.javascriptRouter("jsRoutes")(
-          routes.javascript.Application.stringAs        
+          routes.javascript.Application.colorizeString       
       )
     ).as("text/javascript") 
   }
   
-  def colorize(s: AST.Symbol): NodeSeq = s match {
+  def colorizeExpr(s: AST.Symbol): NodeSeq = s match {
     case AST.Ref(AST.Ident(name))              => Seq(<span class="vname">{name}</span>)
-    case PAST.CApp(AST.Ident(name), pl)        => Seq(<span class="cname">{name}</span>, Text("(")) ++ colorize(pl)
+    case PAST.CApp(AST.Ident(name), pl)        => Seq(<span class="cname">{name}</span>, Text("(")) ++ colorizeExpr(pl)
     case PAST.ParamList(ap, np, isClosed)      => colorizeParams(ap, np) ++ (if(isClosed) Seq(Text(")")) else Seq())
     case PAST.NParamWithEqual(AST.Ident(name)) => Seq(<span class="pname">{name}</span>, Text(" = "))
     case PAST.NParamNoEqual(AST.Ident(name))   => Seq(<span class="pname">{name}</span>)
-    case PAST.NParam_(AST.Ident(name), v)      => Seq(<span class="pname">{name}</span>, Text(" = ")) ++ colorize(v)
+    case PAST.NParam_(AST.Ident(name), v)      => Seq(<span class="pname">{name}</span>, Text(" = ")) ++ colorizeExpr(v)
     case PAST.MissingElem()                    => Seq() 
     case AST.Integer(v)                        => Seq(<span class="integer">{v}</span>)
     case AST.Float(v)                          => Seq(<span class="float">{v}</span>)
@@ -49,11 +53,11 @@ object Application extends Controller {
     case AST.Bool(v)                           => Seq(<span class="boolean">{v}</span>)
     case AST.Seq(seq)                          => Seq(Text("[")) ++ splitSeq(seq, ",") ++ Seq(Text("]"))
     case PAST.LocalValDefsNoIn(defs)           => Seq(<span class="keyword">let</span>, Text(" ")) ++ splitSeq(defs, ";")
-    case PAST.LocalValDefsWithIn(defs)         => colorize(PAST.LocalValDefsNoIn(defs)) ++ Seq(Text(" "), <span class="keyword">in</span>)
-    case PAST.LocalValDefs(defs, expr)         => colorize(PAST.LocalValDefsWithIn(defs)) ++ Seq(Text(" ")) ++ colorize(expr)   
+    case PAST.LocalValDefsWithIn(defs)         => colorizeExpr(PAST.LocalValDefsNoIn(defs)) ++ Seq(Text(" "), <span class="keyword">in</span>)
+    case PAST.LocalValDefs(defs, expr)         => colorizeExpr(PAST.LocalValDefsWithIn(defs)) ++ Seq(Text(" ")) ++ colorizeExpr(expr)   
     case PAST.ValDefNoEqual(AST.Ident(name))   => Seq(<span class="vname">{name}</span>) 
     case PAST.ValDefWithEqual(AST.Ident(name)) => Seq(<span class="vname">{name}</span>, Text(" = "))
-    case PAST.ValDef_(AST.Ident(name), expr)   => Seq(<span class="vname">{name}</span>, Text(" = ")) ++ colorize(expr)
+    case PAST.ValDef_(AST.Ident(name), expr)   => Seq(<span class="vname">{name}</span>, Text(" = ")) ++ colorizeExpr(expr)
   }
   
   def colorizeParams(ap: Seq[AST.Expr], np: Seq[PAST.NParam]): NodeSeq = (ap, np) match {
@@ -65,14 +69,14 @@ object Application extends Controller {
   
   def splitSeq(seq: Seq[AST.Symbol], splitter: String): NodeSeq = seq match {
     case Seq() => Seq()
-    case s::ss if ss.isEmpty => colorize(s)
-    case s::ss => colorize(s) ++ Seq(Text(splitter + " ")) ++ splitSeq(ss, splitter)
+    case s::ss if ss.isEmpty => colorizeExpr(s)
+    case s::ss => colorizeExpr(s) ++ Seq(Text(splitter + " ")) ++ splitSeq(ss, splitter)
   }
   
   def splitNParams(np: Seq[PAST.NParam]): NodeSeq = np match {
     case Seq() => Seq()
-    case n::np if np.isEmpty => colorize(n)
-    case n::np => colorize(n) ++ Seq(Text(", ")) ++ splitNParams(np)
+    case n::np if np.isEmpty => colorizeExpr(n)
+    case n::np => colorizeExpr(n) ++ Seq(Text(", ")) ++ splitNParams(np)
   }
   
 }
