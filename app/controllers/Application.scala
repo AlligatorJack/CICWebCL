06995bdf79fe32e179838a260f12038eb08610bd
Evgeny Novoseltsev <evgeny_novoseltsev@web.de>
2012-10-28 17:37:41 +0100

    highlighting done

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -24,15 +24,31 @@ object Application extends Controller {
   }
 
   def colorizeString(s: String)= Action {
-    val lexer = new PartialCmdLexer(new StringReader(s))
+    //version with lexer
     try {
-      val expr = new PartialCmdParser().parse(lexer).asInstanceOf[AST.Expr]
+      val expr = new PartialCmdParser().parse(new PartialCmdLexer(new StringReader(s))).asInstanceOf[AST.Expr]
       Ok(colorizeExpr(expr).toString)
     }
     catch {
       //there is no exception if beaver can repair the user-string. We need to override Events.syntaxError maybe(just throw an Exception)
-      case e:Exception => Ok(colorizeTokens(new SymbolSeq(lexer)).toString)
+      case e:Exception => Ok(colorizeLexer(new PartialCmdLexer(new StringReader(s))).toString)
     }
+    
+//    //version with SymbolSeq
+//    try {
+//      val expr = new PartialCmdParser().parse(new PartialCmdLexer(new StringReader(s))).asInstanceOf[AST.Expr]
+//      Ok(colorizeExpr(expr).toString)
+//    }
+//    catch {
+//      //there is no exception if beaver can repair the user-string. We need to override Events.syntaxError maybe(just throw an Exception)
+//      case e:Exception => Ok(colorizeTokens(new SymbolSeq(new PartialCmdLexer(new StringReader(s)))).toString)
+//    }
+    
+//    //test for colorizeTokens
+//    Ok(colorizeTokens(new SymbolSeq(new PartialCmdLexer(new StringReader(s)))).toString)
+    
+//    //test for colorizeLexer
+//    Ok(colorizeLexer(new PartialCmdLexer(new StringReader(s))).toString)
   }
   
   def javascriptRoutes = Action { implicit request =>
@@ -43,18 +59,41 @@ object Application extends Controller {
     ).as("text/javascript") 
   }
   
+  def colorizeLexer(lexer: PartialCmdLexer): NodeSeq = {
+    val token = lexer.nextToken()
+    token.getId() match {
+      case 0  => Seq()
+      case 1  => Seq(Text(", ")) ++ colorizeLexer(lexer)
+      case 2  => Seq(Text("] ")) ++ colorizeLexer(lexer)
+      case 3  => Seq(Text(") ")) ++ colorizeLexer(lexer)
+      case 4  => Seq(<span class="keyword">in </span>) ++ colorizeLexer(lexer)
+      case 5  => Seq(Text("; ")) ++ colorizeLexer(lexer)
+      case 6  => Seq(<span class="name">{token.value} </span>) ++ colorizeLexer(lexer)
+      case 7  => Seq(Text("(")) ++ colorizeLexer(lexer)
+      case 8  => Seq(<span class="keyword">let </span>) ++ colorizeLexer(lexer)
+      case 9  => Seq(Text("[")) ++ colorizeLexer(lexer)
+      case 10 => Seq(<span class="integer">{token.value} </span>) ++ colorizeLexer(lexer)
+      case 11 => Seq(<span class="float">{token.value} </span>) ++ colorizeLexer(lexer)
+      case 12 => Seq(<span class="string">"{token.value}" </span>) ++ colorizeLexer(lexer)
+      case 13 => Seq(<span class="boolean">{token.value} </span>) ++ colorizeLexer(lexer)
+      case 14 => Seq(<span class="boolean">{token.value} </span>) ++ colorizeLexer(lexer)
+      case 15 => Seq(Text(" = ")) ++ colorizeLexer(lexer)
+    }
+  }
+  
+  //something is wrong with SymbolSeq
   def colorizeTokens(tokenSeq: SymbolSeq): NodeSeq = {
-    Seq(<span class="keyword">{tokenSeq.length}</span>)
+    tokenSeq.map(x => <span class="keyword">{x.value}</span>)
 //    tokenSeq.flatMap(x => x.getId() match {
 //      case 0  => Seq()
 //      case 1  => Seq(Text(","))
 //      case 2  => Seq(Text("]"))
 //      case 3  => Seq(Text(")"))
-//      case 4  => Seq(<span class="keyword">{x.value}</span>)
+//      case 4  => Seq(<span class="keyword">in</span>)
 //      case 5  => Seq(Text(";"))
 //      case 6  => Seq(<span class="name">{x.value}</span>)
 //      case 7  => Seq(Text("("))
-//      case 8  => Seq(<span class="keyword">{x.value}</span>)
+//      case 8  => Seq(<span class="keyword">let</span>)
 //      case 9  => Seq(Text("["))
 //      case 10 => Seq(<span class="integer">{x.value}</span>) 
 //      case 11 => Seq(<span class="float">{x.value}</span>)
@@ -75,7 +114,7 @@ object Application extends Controller {
     case AST.MissingElem()                    => Seq() 
     case AST.Integer(v)                       => Seq(<span class="integer">{v}</span>)
     case AST.Float(v)                         => Seq(<span class="float">{v}</span>)
-    case AST.String(v)                        => Seq(<span class="string">{v}</span>)
+    case AST.String(v)                        => Seq(<span class="string">"{v}"</span>)
     case AST.Bool(v)                          => Seq(<span class="boolean">{v}</span>)
     case AST.Seq(seq)                         => Seq(Text("[")) ++ splitSeq(seq, ",") ++ Seq(Text("]"))
     case AST.LocalValDefsNoIn(defs)           => Seq(<span class="keyword">let</span>, Text(" ")) ++ splitSeq(defs, ";")
