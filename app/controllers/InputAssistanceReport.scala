63d7d1ccf82f153e864d2bb652dfdb98008ed4a4
SimonKaltenbacher <simonkaltenbacher@googlemail.com>
2012-10-29 14:32:16 +0100

    Implementation of Writer for InputAssistanceReport

--- app/controllers/InputAssistanceReport.scala
+++ app/controllers/InputAssistanceReport.scala
@@ -1,5 +1,10 @@
 package controllers
 
-case class InputAssistanceReport(colered: String, completions: Seq[String], errors: Seq[String]) {
+case class InputAssistanceReport(colered: String, completions: Seq[String], errors: Seq[String])
 
+object JsonWriter {
+
+  implicit object InputAssistanceReportJson extends Writer[InputAssistanceReport] {
+    writes(o: InputAssistanceReport): JsValue = //Add implementation
+  }
 }
