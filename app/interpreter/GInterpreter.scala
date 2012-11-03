637f9571c6daea78ec7fbe9c97e93d7faaf4516e
AlligatorJack <alligator_jack@hotmail.com>
2012-11-03 12:15:13 +0100

    ...

--- app/interpreter/GInterpreter.scala
+++ app/interpreter/GInterpreter.scala
@@ -10,7 +10,6 @@ object GInterpreter extends BaseInterpreter {
     name match {
       case "email" => VAL.Integer(Email.apply(values(0).getBool, values(1).getInt, values(2).getBool))
       case "move" => VAL.String(Move.apply(values(0).getString, values(1).getString))
-      case "fuck" => VAL.Unit(Fuck.apply(values(0).getString, values(1).getString, values(2).getInt, values(3).getBool))
       case "copy" => VAL.String(Copy.apply(values(0).getString, values(1).getString))
     }
 }
